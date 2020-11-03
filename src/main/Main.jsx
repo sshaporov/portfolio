34049ecbe302bf83bd3f2905df4c724f48e4c9ce
Sergey Shaporov <sshaporov86@gmail.com>
2020-11-03 18:40:39 +0300

    init project and css for header and main components

---
+++ src/main/Main.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import styles from "./Main.module.css"
+import stylesContainer from "./../common/styles/Container.module.css"
+
+function Main() {
+    return (
+        <div className={styles.mainBlock}>
+            <div className={stylesContainer.container}>
+                <div className={styles.text}>
+                    <span>Hi there</span>
+                    <h1>I am Sergey Shaporov</h1>
+                    <p>Frontend Developer</p>
+                </div>
+                <div className={styles.photo}></div>
+            </div>
+        </div>
+    );
+}
+
+export default Main;
