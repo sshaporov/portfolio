5a9b5df7b39ad279d3249ec0ba7f7bb4af175cfc
Sergey Shaporov <sshaporov86@gmail.com>
2021-03-05 15:55:47 +0300

    refactoring css

--- src/projects/project/Project.jsx
+++ src/projects/project/Project.jsx
@@ -1,12 +1,11 @@
 import React from "react"
 import styles from "./Project.module.css"
-import Button from "../../common/components/button/Button";
 
 function Project(props) {
     return (
         <div className={styles.project}>
             <div className={styles.imgContainer}>
-                <img className={styles.projectImg}/>
+                {/*<img className={styles.projectImg}/>*/}
                 <a className={styles.button}>Посмотреть</a>
             </div>
             <div className={styles.title}>{props.title}</div>
