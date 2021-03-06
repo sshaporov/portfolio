8a7e97432a9fa4ecf4494e4de002cfd8c76e0ccd
Sergey Shaporov <sshaporov86@gmail.com>
2021-03-06 15:43:20 +0300

    add Skills title

--- src/skills/Skills.jsx
+++ src/skills/Skills.jsx
@@ -2,12 +2,14 @@ import React from "react"
 import styles from "./Skills.module.css"
 import stylesContainer from "../common/styles/Container.module.css"
 import Skill from "./skill/Skill";
+import {Title} from "../common/components/title/Title";
 
 function Skills() {
     return (
         <div className={styles.skillsBlock}>
             <div className={`${styles.skillsContainer} ${stylesContainer.container}`}>
-                <h2 className={styles.title}>Skills</h2>
+                {/*<h2 className={styles.title}>Skills</h2>*/}
+                <Title name={'Skills'}/>
                 <div className={styles.skills}>
                     <Skill title={'JS'} description={'asf asd fasjdfasd fasdfasdfasd fasd fasdf asdfasdfasdf asdfasdfasdf sadfsadf'}/>
                     <Skill title={'CSS'} description={'asf sadfaskdfbaskdf asd fasjdfasd fasdfasdfasd fasd fasdf asdfasdfasdf asdfasdfasdf sadfsadf'}/>
