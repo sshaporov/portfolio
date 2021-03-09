d015053d0a5d019a5ddce640c6f4d06f2df22bd3
Sergey Shaporov <sshaporov86@gmail.com>
2021-03-10 00:43:53 +0300

    add mixins to Skills component

--- src/skills/skill/Skill.jsx
+++ src/skills/skill/Skill.jsx
@@ -1,14 +1,12 @@
 import React from "react"
-import styles from "./Skill.module.css"
+import styles from "./Skill.module.scss"
 
 function Skill(props) {
     return (
         <div className={styles.skill}>
             <div className={styles.icon}></div>
-            <h3 className={styles.title}>{props.title}</h3>
-            <span className={styles.description}>
-                {props.description}
-            </span>
+            <div className={styles.title}>{props.title}</div>
+            <span className={styles.description}>{props.description}</span>
         </div>
     );
 }
