b943a652685f995900385e7150ae37d726d879b4
Sergey Shaporov <sshaporov86@gmail.com>
2021-03-09 21:44:02 +0300

    Contacts css refactoring

--- src/contacts/Contacts.jsx
+++ src/contacts/Contacts.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import styles from "./Contacts.module.scss"
 import stylesContainer from "../common/styles/Container.module.css"
 import {Title} from "../common/components/title/Title"
+import Button from "../common/components/button/Button"
 
 function Contacts() {
     return (
@@ -12,9 +13,9 @@ function Contacts() {
                     <input className={styles.name} type={'text'} placeholder={'Name'}/>
                     <input className={styles.email} type={'text'} placeholder={'Email'}/>
                     <textarea className={styles.message} placeholder={'Message'}/>
-                    <button type={'submit'} className={styles.button}>Отправить</button>
+                    {/*<button type={'submit'} className={styles.button}>Отправить</button>*/}
+                    <Button name={'Send'}/>
                 </form>
-                {/*<Button name={'Отправить'}/>*/}
             </div>
         </div>
     );
