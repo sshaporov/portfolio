23712d5b5f74928270e1352399a144082b721777
Sergey Shaporov <sshaporov86@gmail.com>
2021-03-10 23:52:02 +0300

    icons refactoring

--- src/04-resume/Resume.jsx
+++ src/04-resume/Resume.jsx
@@ -1,33 +1,37 @@
 import React from "react"
 import styles from "./Resume.module.scss"
 import {Title} from "../common/components/title/Title"
+import Button from "../common/components/button/Button";
 
 function Resume() {
     return (
         <div className={styles.resume}>
             <div className={styles.container}>
-                <div className={styles.timelineContent}>
                     <Title name={'Resume'}/>
                     <ul className={styles.timeline}>
                         <li className={styles.event} data-date="2012">
                             <h3>Mostra Group</h3>
                             <p>QA Engineer </p>
+                            <span>Functional testing Web and Desktop software. Test design, test planning. asdfasdfa asdfasdfasdf askdfjhaskjdfhasj asdfhasdjf asdfasdfasdf sadf dsfa sdf sadfasdfasdf</span>
+
                         </li>
                         <li className={styles.event} data-date="2013">
                             <h3>Qulix Systems</h3>
                             <p>Senior QA Engineer</p>
+                            <span>Functional testing Web and Desktop software. Test design, test planning. asdfasdfa asdfasdfasdf askdfjhaskjdfhasj asdfhasdjf asdfasdfasdf sadf dsfa sdf sadfasdfasdf</span>
                         </li>
                         <li className={styles.event} data-date="2018">
                             <h3>Godel Technologies</h3>
                             <p>Senior QA Engineer</p>
+                            <span>Functional testing Web and Desktop software. Test design, test planning. asdfasdfa asdfasdfasdf askdfjhaskjdfhasj asdfhasdjf asdfasdfasdf sadf dsfa sdf sadfasdfasdf</span>
                         </li>
                         <li className={styles.event} data-date="2020">
                             <h3>Godel Technologies</h3>
                             <p>JS Developer</p>
+                            <span>Functional testing Web and Desktop software. Test design, test planning. asdfasdfa asdfasdfasdf askdfjhaskjdfhasj asdfhasdjf asdfasdfasdf sadf dsfa sdf sadfasdfasdf</span>
                         </li>
                     </ul>
-                </div>
-
+                <Button name={'Download CV'}/>
             </div>
         </div>
     );
