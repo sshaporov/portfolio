268d3aa0064d1e1226e50fd3892c0d2b9487640a
Sergey Shaporov <sshaporov86@gmail.com>
2021-03-11 23:28:08 +0300

    impl scroll up way

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Projects from "./05-projects/Projects";
 import Contacts from "./06-contacts/Contacts";
 import Footer from "./07-footer/Footer";
 import Resume from "./04-resume/Resume";
+import ScrollToTop from "react-scroll-up"
+import scroll from './assets/image/icons/scroll.svg'
+import styles from './App.module.scss'
 
 function App() {
     const [burgerIsOpened, setBurgerIsOpened] = useState(false)
@@ -13,15 +16,20 @@ function App() {
         setBurgerIsOpened(!burgerIsOpened)
     }
     return (
-    <div className="App">
-      <Header burgerIsOpened={burgerIsOpened} onBurgerBtnClick={onBurgerBtnClick}/>
-      <Main burgerIsOpened={burgerIsOpened}/>
-      <Skills/>
-      <Resume/>
-      <Projects/>
-      <Contacts/>
-      <Footer/>
-    </div>
+        <div className="App">
+            <Header burgerIsOpened={burgerIsOpened} onBurgerBtnClick={onBurgerBtnClick}/>
+            <Main burgerIsOpened={burgerIsOpened}/>
+            <Skills/>
+            <Resume/>
+            <Projects/>
+            <Contacts/>
+            <Footer/>
+            <ScrollToTop showUnder={160}>
+                <div className={styles.scrollIcon}>
+                    <img src={scroll}/>
+                </div>
+            </ScrollToTop>
+        </div>
   );
 }
 
